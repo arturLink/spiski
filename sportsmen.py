37dc263ddd2b951c3da5642ba03292399027e45a
opilane <opilane@HP-CZC6297B8Z>
2021-12-01 10:20:59 +0200

    did some tasks

--- sportsmen.py
+++ sportsmen.py
@@ -14,21 +14,21 @@
 #print(len(Kmd),"oN JÄÄNUD LISTIS")
 
 #Indeks po4tovii TASK2
-from random import *
-spisok=[]
-N=int(input("N "))
-for i in range(N):
-	spisok.append(randint(1,100))
-print(spisok)
+#from random import *
+#spisok=[]
+#N=int(input("N "))
+#for i in range(N):
+	#spisok.append(randint(1,100))
+#print(spisok)
 ###
-max_=max(spisok)
-print(max_)
-max_2=max(spisok)/len(spisok)
-print(max_2)
-ind=spisok.index(max_)
-spisok.pop(ind)
-spisok.insert(ind,max_2)
-print(spisok)
+#max_=max(spisok)
+#print(max_)
+#max_2=max(spisok)/len(spisok)
+#print(max_2)
+#ind=spisok.index(max_)
+#spisok.pop(ind)
+#spisok.insert(ind,max_2)
+#print(spisok)
 ###
 
 #for s in spisok:
@@ -66,3 +66,60 @@ print(spisok)
 	#print("Jätka kodus")
 #else:
 	#print("Kanna maski")
+
+#task 4
+#from random import *
+#spisok=spisok_=[]
+#N=int(input("Enter number: "))
+#for i in range(N):
+	#spisok.append(randint(-100,100))
+#print(spisok)
+#for n in spisok:
+	#spisok_.append(abs(n))
+#spisok_.sort()
+#print(spisok_)
+#spisok_.sort(reverse=True)
+#print(spisok_)
+
+#task5
+s1=['крот', 'белка', 'выхухоль']
+s2=['a', 'aa', 'aaa', 'aaaa', 'aaaaa']
+s3=['qweasdqweas', 'q', 'rteww', 'ewqqqqq']
+ss=[s1,s2,s3]
+N=0
+while N<3:
+	print(ss[N])
+	pikkem=0
+	sN_=[]
+	for s in ss[N]:
+		pikkus=len(s)
+		if pikkus>pikkem:
+			pikkem=pikkus
+	for s in ss[N]:
+		sN_.append(s.ljust(pikkem,"_"))
+	print(sN_)
+	N+=1
+
+###not the right way###
+#for s in s1:
+	#pikkus=len(s)
+	#if pikkus>pikkem:
+		#pikkem=pikkus
+#for s in s1:
+	#s1_.append(s.ljust(pikkem,"_"))
+	#print(s1_)
+#for s in s2:
+	#pikkus=len(s)
+	#if pikkus>pikkem:
+		#pikkem=pikkus
+#for s in s2:
+	#s2_.append(s.ljust(pikkem,"_"))
+	#print(s2_)
+#for s in s3:
+	#pikkus=len(s)
+	#if pikkus>pikkem:
+		#pikkem=pikkus
+#for s in s3:
+	#s3_.append(s.ljust(pikkem,"_"))
+	#print(s3_)
+#print(ss[0])
